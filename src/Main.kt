98cee9b4f086dacd7adacd5ec46ad34d8cfa43e3
SemGu2 <sem06gu@gmail.com>
2024-10-15 13:58:55 +0300

    Second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,35 @@
 fun main() {
-    println("Hello World!")
-}///
+    val student:Student? = Student()
+    student?.let{ it ->
+        it.hi()
+        it.bye()
+        it.name = "123"
+    }
+    student?.apply {
+        hi()
+        bye()
+    }
+
+    //run
+    val stringStudent = Student().run {
+        hi()
+        bye()
+        lastName //return
+    }
+    println(stringStudent)
+
+    //with
+    val nameStudent = with(Student()){
+        bye()
+        name //return
+    }
+    println(nameStudent)
+
+    //also
+    val StudentQQ = Student().also { st ->
+        st.hi()
+        st.bye()
+        st.name = "111"
+    }
+    println(StudentQQ.name)
+}
---
+++ src/Student.kt
@@ -0,0 +1,13 @@
+class Student {
+    var name:String = "Sasha"
+    val lastName:String = "LolKek"
+
+    fun hi()
+    {
+        println("hi")
+    }
+    fun bye()
+    {
+        println("bye")
+    }
+}
